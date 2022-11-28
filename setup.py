ae99adba27eb9acde8b8b34fae35f128b40ed841
TheOGUmar <the.og.umar@gmail.com>
2022-11-29 01:49:18 +0530

    added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Talk',
+      description='A wrapper for openai/whisper'
+      author='TheOGUmar'
+      author_email='the.og.umar@gmail.com'
+      packages=['whisper', 'python3', 'torch', 'ffmpeg-python', 'transformers']
+      )
