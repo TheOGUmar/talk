4f1ad398a4f6ecaa0da1708a4faef0050abc6b94
TheOGUmar <the.og.umar@gmail.com>
2022-11-29 02:15:19 +0530

    added input to file

--- talk.py
+++ talk.py
@@ -2,14 +2,16 @@ import whisper
 
 model = whisper.load_model("tiny.en")
 
+audio_file = input("Pick a file: ")
+
 # Load and trim audio to be under 30 seconds (encodes the audio)
-audio = whisper.load_audio("convo_under_30.mp3")
+audio = whisper.load_audio(audio_file)
 audio = whisper.pad_or_trim(audio)
 
 mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
 # decode audio
-options = whisper.DecodingOptions(language=en, fp16=False)
+options = whisper.DecodingOptions(language="en", fp16=False)
 result = whisper.decode(model, mel, options)
 
 print(result.text)
